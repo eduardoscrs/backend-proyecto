7ab488a4698be0063df523b90607e50764c61270
Eduardo Escares <eduardoscrs60@gmail.com>
2023-11-03 03:20:59 -0300

    ahora si (?, espero

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import express from "express"
-import userRoutes from "./routes/usuarios.routes.js"
-import rutasproducto from "./routes/productos.routes.js"
-const PORT = 3000
+import express from "express";
+import userRoutes from "./routes/usuarios.routes.js";
+import rutasproducto from "./routes/productos.routes.js";
 
-const app = express()
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use('/api',userRoutes)
-app.use('/api',rutasproducto)
+const app = express();
 
-app.use((req,res,next)=>{
-    res.status(404).json({message:"Endpoint not found"})
-})
+app.use(express.json());
+app.use('/api', userRoutes);
+app.use('/api', rutasproducto);
 
-app.listen(3000)
-console.log(`http://localhost:${PORT}/api/usuarios`)
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Endpoint not found" });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log("Server is running on http://localhost:${PORT}/api/usuarios");
+});

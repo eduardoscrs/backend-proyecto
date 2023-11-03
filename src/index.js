10550ed5f2102db2221c20d1e7f70639d7cd7161
Eduardo Escares <eduardoscrs60@gmail.com>
2023-11-03 03:48:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 import userRoutes from "./routes/usuarios.routes.js";
 import rutasproducto from "./routes/productos.routes.js";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// Utilizar middleware de CORS
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', userRoutes);
